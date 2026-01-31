0bef370556dcfa5a1e3264e815fdb9605524973a
jodeka <gnarf2-d2@posteo.de>
2026-01-31 12:51:04 +0100

    player simple dash

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,13 +8,22 @@ class_name Player
 @export var attack_damage = 10
 @export var health_component: HealthComponent
 @export var attack_shape: Attack
+@export var dash_speed = 2.5
+@export var dash_dur = .8
 
 var facing_direction = 1
+var speed_mod = 1
+var dash_time = 0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left","right", "up", "down")
-	global_position += direction * speed * delta
-		
+	var actual_speed = speed
+	if dash_time >= 0:
+		actual_speed *= dash_speed
+		dash_time -= delta
+	
+	global_position += direction * actual_speed * delta
+	
 	if direction.length() > 0.1:
 		if direction.x > 0:
 			facing_direction = -1
@@ -22,7 +31,6 @@ func _physics_process(delta: float) -> void:
 			facing_direction = 1
 		scale.x = facing_direction
 		queue_redraw()
-
 	
 func _ready() -> void:
 	hit_box.area_entered.connect(on_took_damage)
@@ -39,7 +47,8 @@ func is_attacking() -> bool:
 
 ## TODO: Make the player dash forward, and become invincible
 func do_dash():
-	pass
+	health_component.i_frames = dash_dur
+	dash_time = dash_dur
 	
 func do_attack():
 	attack_animation.play("attack", -1, attack_speed)
